63411e69948aef99027bf5b68f67c28bed245db9
Krishna Chaiatanya <ekrishnachaitanya2004@gmail.com>
2022-10-29 20:51:52 +0530

    Update Automate.py

--- Automate.py
+++ Automate.py
@@ -1,10 +1,12 @@
+#ekc2004
 import random
+#random library can pick random strings that are defined in program
 import pyautogui as pg
+#pyautogui is a python libray that can write,press the letters as user typed.
 import time
-#apran_khunger
 animal = ('Dog', 'Monkey', 'Cow', 'Buffalow' , 'Elephant', 'Donkey', 'Cat', )
 time.sleep(10)
-#apran_khunger
+#here sleep time is set to 10sec,because some defenders can find it as bot
 for i in range (1000):
     a  = random.choice(animal)
     pg.write('You Are a ' + a)
